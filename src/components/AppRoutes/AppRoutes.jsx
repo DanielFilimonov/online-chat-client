f987409e977fd79a23a57d31f7292493c082090b
DanielFilimonov <marseanen85@gmail.com>
2025-11-18 14:48:32 +0300

    Первая версия

---
+++ src/components/AppRoutes/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Main from "../Main/Main";
+import Chat from "../Chat/Chat";
+
+const AppRoutes = () => (
+	<Routes>
+		<Route path="/" element={<Main />} />
+		<Route path="/chat" element={<Chat />} />
+	</Routes>
+);
+
+export default AppRoutes;
